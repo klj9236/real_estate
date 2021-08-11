ed524a48672ae0be9d36f12c2df718c5e951d922
devincremeans <86858013+devincremeans@users.noreply.github.com>
2021-08-10 21:30:06 -0400

    revised_code_only

---
+++ revised.py
@@ -0,0 +1,92 @@
+
+# Imports 
+import requests
+import json
+from pprint import pprint
+from pandas import DataFrame
+
+url = "https://us-real-estate.p.rapidapi.com/sold-homes"
+
+querystring = {"state_code":"NY","city":"Latham","location":"12110","limit":"20","offset":"0","sort":"sold_date"}
+
+headers = {
+    'x-rapidapi-key': "8352fae3e7mshe6a66a045b0c651p1121bcjsn9202ff5c9e7c", # Needs to be changed to an enviornment varaible, PLease don't use my API Key :(
+    'x-rapidapi-host': "us-real-estate.p.rapidapi.com"
+    }
+
+response = requests.request("GET", url, headers=headers, params=querystring)
+
+r = response.text
+parsed_response = json.loads(r)
+
+
+
+clean_list = []
+for item in parsed_response["data"]["results"]:
+
+  clean_item = {
+      "listing_id":item["listing_id"],
+      "lat":item["location"]["address"]["coordinate"]['lat'],
+      "long":item["location"]["address"]["coordinate"]['lon'],
+      "property_type":item["description"]["type"],
+      "street_address":item["location"]["address"]["line"],
+      "city":item["location"]["address"]["city"],
+      "state":item["location"]["address"]["state"],
+      "zip_code":item["location"]["address"]["postal_code"],
+      "square_feet":item["description"]["sqft"],
+      "beds":item["description"]["beds"],
+      "full_baths":item["description"]["baths_full"],
+      "half_baths":item["description"]["baths_half"],
+      "garage":item["description"]["garage"],
+      "lot_size_sf":item["description"]["lot_sqft"],
+      "year_built":item["description"]["year_built"],
+      "list_price":item["list_price"],
+      "sold_price":item["description"]["sold_price"],
+      "list_date":item["list_date"],
+      "sold_date":item["description"]["sold_date"],
+      "primary_phot_url":None,
+      "url_photo_1":None,
+      "url_photo_2":None,
+      "url_photo_3":None,
+      "url_photo_4":None,
+      "url_photo_5":None,
+      
+  }
+
+  try:
+    clean_item["primary_phot_url"] = item["primary_photo"]["href"]
+  except:
+    pass
+
+  try:
+    clean_item["url_photo_1"] = item["photos"][0]["href"]
+  except:
+    pass
+  
+  try:
+    clean_item["url_photo_2"] = item["photos"][1]["href"]
+  except:
+    pass
+
+  try:
+    clean_item["url_photo_3"] = item["photos"][2]["href"]
+  except:
+    pass
+
+  try:
+    clean_item["url_photo_4"] = item["photos"][3]["href"]
+  except:
+    pass
+
+  try:
+    clean_item["url_photo_5"] = item["photos"][4]["href"]
+  except:
+    pass
+
+  clean_list.append(clean_item)
+
+df = DataFrame(clean_list)
+
+df.to_csv("home_price_results.csv")
+
+print(df)
