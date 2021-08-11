58cb3f7a13664d764b233ba390186a107ddd7fec
klj9236 <87046019+klj9236@users.noreply.github.com>
2021-08-11 19:56:43 -0400

    Update real_estate_test.py

--- test/real_estate_test.py
+++ test/real_estate_test.py
@@ -1,11 +1,10 @@
 
 from app.real_estate import fetch_soldhomes
 
-total_cols=len(df.axes[1])
 
 def function_type():
-    results = len(fetch_soldhomes(State="MI",City="Detroit",Zipcode="48205"))
-    assert results == int(25)
+    results = fetch_soldhomes(State="MI",City="Detroit",Zipcode="48205")
+    assert len(results) == int(25)
 
     
    
